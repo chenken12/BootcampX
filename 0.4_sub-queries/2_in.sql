be90c6ba04a3999aaa86eeee22a523505a40030a
Kenneth Chen <kenchen0606@gmail.com>
2021-11-23 22:57:16 +0000

    add teacher and request table and seeds

---
+++ 0.4_sub-queries/2_in.sql
@@ -0,0 +1,18 @@
+SELECT assignments.name
+FROM assignments 
+WHERE id NOT IN
+(
+  SELECT assignment_id
+  FROM assignment_submissions
+  JOIN students ON students.id = student_id
+  WHERE students.name = 'Ibrahim Schimmel'
+);
+
+/*
+SELECT * FROM table
+WHERE id IN (
+  SELECT something_id
+  FROM someTable
+  WHERE something
+);
+*/
